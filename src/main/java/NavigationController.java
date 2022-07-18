3165ba32427a03e0c34fba113f4db0cf1654e13d
Joris TEXEREAU <joris-texereau_student2022@wilder.school>
2022-07-18 14:28:55 +0200

    exo nav conditionnelle fini

--- src/main/java/NavigationController.java
+++ src/main/java/NavigationController.java
@@ -16,14 +16,33 @@ import javax.faces.bean.ManagedProperty;
 @ManagedBean(name = "navigationController", eager = true)
 public class NavigationController {
 
+    @ManagedProperty(value= "#{param.pageIdentifiant}")
+    private String pageId;
+
     /**
      * Creates a new instance of NavigationController
      */
     public NavigationController() {
     }
-    
+
     public String moveToHelloPage() {
         return "hello";
     }
+
+    public String goToPage() {
+        if (pageId != null){
+            return pageId;
+        }
+        return "home";
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
+    }
+
     
 }
